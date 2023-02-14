c773fe69bb091df813c819ea6e9ae2ce70bead33
Rull Deef <deeroll666@gmail.com>
2023-02-14 11:10:46 +0300

    add lab1

---
+++ labs/lab_1/app/src/main/java/explan/ModelingResult.java
@@ -0,0 +1,11 @@
+package explan;
+
+public class ModelingResult {
+    public float rhoTheoretical;
+    public float rhoPractical;
+
+    public ModelingResult(float rhoTheoritical, float rhoPractical) {
+        this.rhoTheoretical = rhoTheoritical;
+        this.rhoPractical = rhoPractical;
+    }
+}
---
+++ labs/lab_1/app/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module explan {
+    requires transitive javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens explan to javafx.fxml;
+    exports explan;
+}
